96f2bf909040229113248fc5fda06b15b45d7fd5
Frédéric Menou <frederic.menou@gmail.com>
2023-10-07 02:13:41 +0200

    Provide packages for frontend

--- flake.nix
+++ flake.nix
@@ -7,18 +7,28 @@
     nixpkgs-22_11.url = "github:nixos/nixpkgs/release-22.11";
     flake-utils.url = "github:numtide/flake-utils";
     node2nix-hs.url = "github:ptitfred/node2nix-hs/v0.0.1.0";
+    gitignore.url = "github:hercules-ci/gitignore.nix";
+    easy-purescript-nix.url = "github:justinwoo/easy-purescript-nix";
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, nixpkgs-22_11, flake-utils, node2nix-hs, ... }:
+  outputs = { nixpkgs, nixpkgs-unstable, nixpkgs-22_11, flake-utils, node2nix-hs, gitignore, easy-purescript-nix, ... }:
     let globals = {
           lib.valdaro = import ./lib.nix;
 
-          overlays.valdaro = nixpkgs.lib.fixedPoints.composeManyExtensions [ node2nix-hs.overlays.default frontendOverlay haskellOverlay lintersOverlay ];
+          overlays.valdaro = nixpkgs.lib.fixedPoints.composeManyExtensions [ node2nix-hs.overlays.default otherPackages frontendOverlay haskellOverlay lintersOverlay ];
 
           nixosModules.postgresql = import nixos/postgresql.nix;
         };
 
-        frontendOverlay = import pkgs/overlay.nix;
+        otherPackages = import pkgs/overlay.nix;
+
+        frontendOverlay = _: prev: {
+          valdaro.frontend = {
+            inherit (gitignore.lib) gitignoreSource;
+            easy-ps = easy-purescript-nix.packages.${prev.system};
+            inherit (nixpkgs-unstable.legacyPackages.${prev.system}) esbuild;
+          };
+        };
 
         haskellOverlay = globals.lib.valdaro.mkHaskellOverlay {
           extraDepsRoot = pkgs/haskell;
@@ -27,17 +37,19 @@
         };
 
         lintersOverlay = _: prev: {
-          valdaro.lint-nix = (import nixpkgs-22_11 { inherit (prev) system; }).callPackage ./lint.nix {
-            inherit (nixpkgs-unstable) lib;
-          };
+          valdaro = prev.valdaro // {
+            lint-nix = (import nixpkgs-22_11 { inherit (prev) system; }).callPackage ./lint.nix {
+              inherit (nixpkgs-unstable) lib;
+            };
 
-          valdaro.mkChecks =
-            let mkCheck = name: script:
-                  prev.runCommand name {} ''
-                    mkdir -p $out
-                    ${script}
-                  '';
-             in prev.lib.attrsets.mapAttrs mkCheck;
+            mkChecks =
+              let mkCheck = name: script:
+                    prev.runCommand name {} ''
+                      mkdir -p $out
+                      ${script}
+                    '';
+               in prev.lib.attrsets.mapAttrs mkCheck;
+          };
         };
 
      in globals // flake-utils.lib.eachDefaultSystem (system:
