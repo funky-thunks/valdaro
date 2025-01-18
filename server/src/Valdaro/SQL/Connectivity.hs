606b92c9cac8b6603c6a366dac8c7497d66ddb07
Frédéric Menou <frederic.menou@gmail.com>
2025-01-18 20:48:57 +0100

    Fix compilation
    Looks like it never compiled properly :/

--- server/src/Valdaro/SQL/Connectivity.hs
+++ server/src/Valdaro/SQL/Connectivity.hs
@@ -8,13 +8,14 @@ module Valdaro.SQL.Connectivity
   , testConnection
   ) where
 
+import           Control.Monad.IO.Unlift      (MonadUnliftIO, withRunInIO)
 import           Control.Monad.Trans.Control  (MonadBaseControl)
 import           Data.Pool                    (withResource)
 import           Database.PostgreSQL.Simple   (Connection, Only)
 import qualified Database.PostgreSQL.Transact as DBT
 import           Valdaro.SQL.Configuration
 
-type CanSQL r m = (MonadReader r m, HasSQLRuntime r, MonadBaseControl IO m, MonadIO m)
+type CanSQL r m = (MonadReader r m, HasSQLRuntime r, MonadBaseControl IO m, MonadIO m, MonadUnliftIO m)
 
 runDB :: CanSQL r m => DBT.DBT m result -> m result
 runDB action = withConnection (DBT.runDBTSerializable action)
@@ -22,7 +23,8 @@ runDB action = withConnection (DBT.runDBTSerializable action)
 withConnection :: CanSQL r m => (Connection -> m result) -> m result
 withConnection action = do
   SQLRuntime { pool } <- asks getSQLRuntime
-  withResource pool action
+  withRunInIO $ \ runInIO ->
+    withResource pool (runInIO . action)
 
 testConnection :: CanSQL r m => m Bool
 testConnection =
